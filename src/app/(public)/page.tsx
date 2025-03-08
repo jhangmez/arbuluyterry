d4b4e5cf42dd9a82b4c8750c3004d9a8ebb2d494
jhann <jhangomez25@gmail.com>
2025-03-08 17:35:56 -0500

    Se agrego cta section

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,6 +4,7 @@ import { Footer } from "@/components/(public)/footer";
 import { WhatsAppButton } from "@/components/(public)/landingpage/whatsapp-button";
 import { ProyectosSection } from "@/components/(public)/landingpage/proyectos";
 import Profesionales from "@/components/(public)/profesionales";
+import CtaSection from "@/components/(public)/landingpage/CtaSection";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       </main>
       <Profesionales />
       <ProyectosSection />
+      <CtaSection />
       <Footer />
       <WhatsAppButton />
     </section>
